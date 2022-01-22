d181259c3ebb23882829807650b42cffa4da9294
Joel <joel.s.sheng@gmail.com>
2022-01-21 16:27:38 -0800

    Initial commit

---
+++ src/Components/Resume.js
@@ -0,0 +1,121 @@
+import React, { Component } from "react";
+import Slide from "react-reveal";
+
+class Resume extends Component {
+  /*getRandomColor() {
+    let letters = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }*/
+  getRandomColor() {
+    let h = 360 * Math.random();
+    let s = 25 + 70 * Math.random();
+    let l = (55 + 25 * Math.random()) / 100;
+    const a = s * Math.min(l, 1 - l) / 100;
+    const f = n => {
+      const k = (n + h / 30) % 12;
+      const color = l - a * Math.max(Math.min(k - 3, 9 - k, 1), -1);
+      return Math.round(255 * color).toString(16).padStart(2, '0');   // convert to Hex and prefix "0" if needed
+    };
+    return `#${f(0)}${f(8)}${f(4)}`;
+  }
+
+  render() {
+    if (!this.props.data) return null;
+
+    const skillmessage = this.props.data.skillmessage;
+    const education = this.props.data.education.map(function (education) {
+      return (
+        <div key={education.school}>
+          <h3>{education.school}</h3>
+          <p className="info">
+            {education.degree} <span>&bull;</span>
+            <em className="date">{education.graduated}</em>
+          </p>
+          <p>{education.description}</p>
+        </div>
+      );
+    });
+
+    const work = this.props.data.work.map(function (work) {
+      return (
+        <div key={work.company}>
+          <h3>{work.company}</h3>
+          <p className="info">
+            {work.title}
+            <span>&bull;</span> <em className="date">{work.years}</em>
+          </p>
+          <p>{work.description}</p>
+        </div>
+      );
+    });
+
+    const skills = this.props.data.skills.map((skills) => {
+      const backgroundColor = this.getRandomColor();
+      const className = "bar-expand " + skills.name.toLowerCase();
+      const width = skills.level;
+
+      return (
+        <li key={skills.name}>
+          <span style={{ width, backgroundColor }} className={className}></span>
+          <em>{skills.name}</em>
+        </li>
+      );
+    });
+
+    return (
+      <section id="resume">
+        <Slide left duration={1300}>
+          <div className="row education">
+            <div className="three columns header-col">
+              <h1>
+                <span>Education</span>
+              </h1>
+            </div>
+
+            <div className="nine columns main-col">
+              <div className="row item">
+                <div className="twelve columns">{education}</div>
+              </div>
+            </div>
+          </div>
+        </Slide>
+
+        <Slide left duration={1300}>
+          <div className="row work">
+            <div className="three columns header-col">
+              <h1>
+                <span>Work</span>
+              </h1>
+            </div>
+
+            <div className="nine columns main-col">{work}</div>
+          </div>
+        </Slide>
+
+        <Slide left duration={1300}>
+          <div className="row skill">
+            <div className="three columns header-col">
+              <h1>
+                <span>Skills</span>
+              </h1>
+            </div>
+
+            <div className="nine columns main-col">
+              <p>{skillmessage}</p>
+
+              <div className="bars">
+                <ul className="skills">{skills}</ul>
+              </div>
+            </div>
+          </div>
+        </Slide>
+      </section>
+    );
+  }
+}
+
+export default Resume;
